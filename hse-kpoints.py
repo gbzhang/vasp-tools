f96d728db19b0445260161e58b21683fa61129f3
Danny Rehn <danny.rehn@gmail.com>
2018-05-24 06:14:44 -0700

    Update hse kpoints file

--- hse-kpoints.py
+++ hse-kpoints.py
@@ -1,8 +1,9 @@
 from pylab import *
 import os
 
-ibzkpt = "../1-pbe/IBZKPT"
+ibzkpt = "../4-pbe-coarse/IBZKPT"
 os.system("cat " + ibzkpt + " > KPOINTS")
+
 
 f = open("../2-pbe-bands/OUTCAR", 'r')
 
@@ -15,3 +16,19 @@ while True:
 			os.system("echo " + l + " >> KPOINTS")
 			nl = f.readline()
 		break
+f.close()
+
+f = open('KPOINTS', 'r')
+lines = f.readlines()
+f.close()
+
+
+slines = []
+[slines.append(line) for line in lines if len(line.split()) == 4 ]
+nk = len(slines)
+lines[1] = '     ' + str(nk) + '\n'
+
+f = open('KPOINTS', 'w')
+f.write("".join(lines))
+f.close()
+
